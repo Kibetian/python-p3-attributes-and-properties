dbae2c3423bffd3e92bf8e1dd992d727ec975c98
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-08-22 16:40:24 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,27 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self):
+        self._name = ""
+        self._breed = ""
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+ 
+    def breed(self):
+        return self._breed
+
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,25 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self):
+        self._name = ""
+        self._job = ""
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    def job(self):
+        return self._job
+
+    def job(self, value):
+        if value in Person.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
